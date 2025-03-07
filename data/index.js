7e5958ba0cc4f933586edf01e19140adc9611e01
Daniel Trujillo <danny@MacBook-Pro-de-Daniel.local>
2025-03-07 07:40:18 -0600

    Commit inicial Tarea Liga Nacional

---
+++ data/index.js
@@ -0,0 +1,84 @@
+{
+    "equipos": [
+        {
+            "nombre": "Olimpia",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Motagua",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Genesis",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Olancho FC",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Lobos",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Victoria",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Vida",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Maraton",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Real España",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        },
+        {
+            "nombre": "Atletico Choloma",
+            "jugados": 0,
+            "ganados": 0,
+            "perdidos": 0,
+            "empatados": 0,
+            "puntos": 0
+        }
+    ]
+}
